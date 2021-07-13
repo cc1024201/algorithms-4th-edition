d7944c560082004c3d32083a9a1227295f1080d7
Cc <cc1024201@gmail.com>
2021-07-13 23:05:17 +0800

    打印出一个M行N列的二维数组的转置（交换行和列

---
+++ src/main/my/SwapRowsColumns.java
@@ -0,0 +1,20 @@
+/**
+ * @author zhcao
+ */
+public class SwapRowsColumns {
+    private static void convert(int[][] a) {
+        int N = a.length;
+        int M = a[0].length;
+        for (int j = 0; j < M; j++) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] a = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+        convert(a);
+    }
+}
